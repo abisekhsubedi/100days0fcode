bb1b22a7f91fc9defc6ba51276ccdd77edc5f1aa
Abisekh Subedi <abisekhsubedi8765@gmail.com>
2020-12-09 20:42:38 +0545

    improvement

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+
 setInterval(() => {
     let d = new Date();
-    // console.log(d);
-    let  htime = d.getHours();
+   
+    let htime = d.getHours();
     let mtime = d.getMinutes();
     let stime = d.getSeconds();
-    // console.log(htime, mtime, stime);
+
+    // code the logic
+    // all our contents go here. 
+    // For hour hand -> 30h + (m/2) + (s/120)
+    // For minute hand -> 6m + (s/10)
+    // For second hand -> 6s
     let hrotation = (30 * htime) + (mtime/2) + (stime/120);
     let mrotation = (6* mtime) + (stime/10);
     let srotation = 6 * stime;
-    // mrotation, srotation;
        
     // implement our rotation.
     hour.style.transform = `rotate(${hrotation}deg)`;
@@ -16,10 +21,9 @@ setInterval(() => {
     second.style.transform = `rotate(${srotation}deg)`;
 
     // tick sound every second
- 
     function playAudio(url){
         new Audio(url).play();
     }
-playAudio("tick.mp3");
+    playAudio("tick.mp3");
 }, 1000);
 
